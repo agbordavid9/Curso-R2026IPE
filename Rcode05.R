fb70e595b6da402af4ee1be84e68671022891501
NatureProtectionDFF <107116037+NatureProtectionDFF@users.noreply.github.com>
2026-05-05 17:08:33 +0200

    Add settings on plot
    Add color and size

--- Rcode05.R
+++ Rcode05.R
@@ -3,6 +3,6 @@ library(tidyverse)
 dt <- penguins
 
 ggplot(dt, aes(x = body_mass, y = bill_len)) +
-  geom_point() +
-  geom_line() +
+  geom_point(color = "blue") +
+  geom_line(size = 0.5) +
   theme_classic() 
